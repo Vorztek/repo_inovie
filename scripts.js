b3c099d0c88eb06a4eb8dee7c1b7fdcdddf2c73e
SUTRA Theo <theo.sutra@inovie.fr>
2024-05-15 17:16:01 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,41 @@
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    const filiales = [
+        { name: 'Labosud', url: 'https://s117.hexalis.net:2000/', imageUrl: 'images/labosud.jpg' },
+        { name: 'Biomédilab', url: 'https://3027122.hexalis.net:2000/', imageUrl: 'images/Biomedilab.png' },
+    ];
+
+    const filialeList = document.getElementById('filialeList');
+    const searchInput = document.getElementById('searchInput');
+
+    function displayFiliales(filiales) {
+        filialeList.innerHTML = '';
+        filiales.forEach(filiale => {
+            const li = document.createElement('li');
+            const a = document.createElement('a');
+            const img = document.createElement('img');
+            const span = document.createElement('span');
+
+            a.href = filiale.url;
+            img.src = filiale.imageUrl;
+            img.alt = filiale.name;
+            span.textContent = filiale.name;
+
+            a.appendChild(img);
+            a.appendChild(span);
+            li.appendChild(a);
+            filialeList.appendChild(li);
+        });
+    }
+
+    searchInput.addEventListener('input', function () {
+        const searchTerm = searchInput.value.toLowerCase();
+        const filteredFiliales = filiales.filter(filiale =>
+            filiale.name.toLowerCase().includes(searchTerm)
+        );
+        displayFiliales(filteredFiliales);
+    });
+
+    displayFiliales(filiales);
+});
